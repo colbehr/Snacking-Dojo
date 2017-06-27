52f29309731145e9e1c55f2b988c07e2e73c5fcd
dusty-g <dustygalindo@gmail.com>
2017-06-27 11:27:10 -0700

    added db service

--- public/src/app/snackingdojo/snackingdojo.component.ts
+++ public/src/app/snackingdojo/snackingdojo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from "./../database.service"
 
 @Component({
   selector: 'app-snackingdojo',
@@ -6,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./snackingdojo.component.css']
 })
 export class SnackingdojoComponent implements OnInit {
-
-  constructor() { }
+  productList = []
+  constructor(private _dbService: DatabaseService) { }
 
   ngOnInit() {
   }
