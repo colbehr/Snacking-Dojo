104029ba3cdf1d23f10b666312e36085c6bce743
kevinsosborne <kevinsosborne@gmail.com>
2017-06-29 13:32:59 -0700

    added admin

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from "./login/login.component"
 import { SnackingdojoComponent } from "./snackingdojo/snackingdojo.component"
 import { DetailsComponent } from "./snackingdojo/details/details.component"
+import { AdminComponent } from "./admin/admin.component"
 
 const routes: Routes = [
     { path: "", component: SnackingdojoComponent },
     { path: "details/:id", component: DetailsComponent },
+    { path: "admin", component: AdminComponent }
     // { path: "", pathMatch: "full", redirectTo: "/questions" }
 ];
 @NgModule({
