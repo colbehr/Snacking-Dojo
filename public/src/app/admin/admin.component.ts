1bc7bed79fd6446577e545fc963b0bfd5e9e0e46
dusty-g <dustygalindo@gmail.com>
2017-07-05 11:22:01 -0700

    .

--- public/src/app/admin/admin.component.ts
+++ public/src/app/admin/admin.component.ts
@@ -12,13 +12,26 @@ import {GithubUser} from "./../github-user"
 })
 export class AdminComponent implements OnInit {
   productList: Product[]
-
+  user: User
   constructor(private _dbService: DatabaseService, private _router: Router ) { }
 
   ngOnInit() {
     this._dbService.checkStatus().then((githubUser)=>{
 
-      console.log(githubUser)
+
+      if(githubUser){
+        console.log(githubUser)
+        this._dbService.getOneUser({github_id: githubUser.id}).then((user)=>{
+          this.user = user
+          if(user.isAdmin){
+            console.log("welcome admin")
+          }else{
+            this._router.navigate(["/"])
+          }
+        }).catch((err)=>{
+
+          console.log(err)})
+      }
     }).catch((error)=>{
       console.log(error)
     })
