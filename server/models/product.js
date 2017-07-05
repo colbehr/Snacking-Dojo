1d1f6095c991348c58f2fd0c52e0dcd294422d11
dusty-g <dustygalindo@gmail.com>
2017-07-05 13:45:52 -0700

    .

--- server/models/product.js
+++ server/models/product.js
@@ -17,7 +17,7 @@ let ProductSchema = mongoose.Schema({
   },
   price:{
     type: String
-  }
+  },
   comments: [{
     comment: String, _user: {type: mongoose.Schema.Types.ObjectId, ref: "User"}, createdAt: {type: Date}
   }, ]
